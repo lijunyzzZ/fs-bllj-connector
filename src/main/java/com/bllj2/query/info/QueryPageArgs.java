1af97b4308ee9a95f01a7df6bb0a7233b747c3aa
lijuny <1920271554@qq.com>
2020-12-10 21:30:27 +0800

    解决了分页查询的bug

--- src/main/java/com/bllj2/query/info/QueryPageArgs.java
+++ src/main/java/com/bllj2/query/info/QueryPageArgs.java
@@ -1,19 +1,16 @@
 package com.bllj2.query.info;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 /**
  *
  * 封装分页查询的参数
  * @author lijuny
  */
-@Setter
-@Getter
+
+
 @ToString
-@AllArgsConstructor
 public class QueryPageArgs {
 	/**
 	 * 分页页码
@@ -40,4 +37,69 @@ public class QueryPageArgs {
 
 	private String contractNumber;
 
+	public QueryPageArgs(int pageNum, int pageSize, long startTime, long endTime, String accountNo, String poNo, String contractNumber) {
+		this.pageNum = Math.max(1, pageNum);
+		this.pageSize = Math.max(1, pageSize);
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.accountNo = accountNo;
+		this.poNo = poNo;
+		this.contractNumber = contractNumber;
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = Math.max(1,pageNum);
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = Math.max(1, pageSize);
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getPoNo() {
+		return poNo;
+	}
+
+	public void setPoNo(String poNo) {
+		this.poNo = poNo;
+	}
+
+	public String getContractNumber() {
+		return contractNumber;
+	}
+
+	public void setContractNumber(String contractNumber) {
+		this.contractNumber = contractNumber;
+	}
 }
